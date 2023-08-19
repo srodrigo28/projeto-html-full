d8ed705898c19ec51b6f135d0c37e6181fa77c3b
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-08-19 20:22:46 -0300

    capitulo 1 finalizado e listando da api json

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 const tabela = document.querySelector('[data-tabela]')
-const http = new XMLHttpRequest()
 const baseUrl = 'http://localhost:3000/categoria'
 
-const criaNovaLinha = (nome, descricao) => {
+const criaNovaLinha = (id, nome, descricao) => {
     const linhaNovaCliente = document.createElement('tr')
     const conteudo = `
+            <td class="td text-center" data-td> ${id}      </td>
             <td class="td" data-td> ${nome}      </td>
             <td class="td" data-td> ${descricao} </td>
-            <td>
-                <a href="#"> <button onclick='editar()'>Editar </button> </a>
-                <a href="#"> <button onclick='ecluir()'>Excluir</button> </a>
+            <td class="text-center">
+                <a href="#"> 
+                    <button class="btn btn-warning" onclick='editar()'>
+                        Editar 
+                    </button>
+                </a>
+                <a href="#"> 
+                    <button class="btn btn-danger" onclick='ecluir()'>
+                        Excluir
+                    </button>
+                </a>
             </td>
         `
     linhaNovaCliente.innerHTML = conteudo;
@@ -24,15 +32,29 @@ function ecluir(){
     alert('excluíndo')
 }
 
-http.open('GET', baseUrl)
+const listaClientes = () => {
+    const promise = new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest()
+        http.open('GET', baseUrl)
 
-http.send()
-
-http.onload = () => {
-    const data = JSON.parse(http.response)
-    console.log(data)
-
-    data.forEach(element => {
-        tabela.appendChild(criaNovaLinha(element.nome, element.descricao))
-    });
+        http.onload = () => {
+            if(http.status >= 400){
+                reject(JSON.parse(http.response))
+            }else{
+                resolve(JSON.parse(http.response))
+            }
+        }     
+        http.send()
+    })
+    // console.log(promise)
+    return promise
 }
+
+listaClientes()
+.then( data => {
+    data.forEach(element => {
+        tabela.appendChild(criaNovaLinha(
+            element.id, element.nome, element.descricao
+        ))
+    });
+})
